e664740bdb5dfc811d111b1009124b18ec31f277
john-ver <46668752+john-ver@users.noreply.github.com>
2022-04-01 21:55:03 +0900

    Update BatchRedeemVoucher.sol

--- contracts/BatchRedeemVoucher.sol
+++ contracts/BatchRedeemVoucher.sol
@@ -3,15 +3,13 @@ pragma experimental ABIEncoderV2;
 
 import "./interfaces/IKApmNftVoucher.sol";
 import "./klaytn-contracts/ownership/Ownable.sol";
-import "./klaytn-contracts/token/KIP37/IERC1155Receiver.sol";
+import "./klaytn-contracts/token/KIP37/KIP37Holder.sol";
 
 /*
     contract for only testing.
     If you deposit an asset that is not specified in this contarct, it cannot be refund.
 */
-contract BatchRedeemVoucher is Ownable{
-    bytes4 private constant _KIP37_RECEIVED = 0xe78b3325;
-    bytes4 private constant _ERC1155_RECEIVED = 0xf23a6e61;
+contract BatchRedeemVoucher is Ownable, KIP37Holder{
 
     IKApmNftVoucher NFTVoucher;
     constructor(address _nftVoucher) public {
