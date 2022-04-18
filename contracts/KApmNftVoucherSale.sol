9852e82042d907da53c6b1513203fc733426e77a
john-ver <46668752+john-ver@users.noreply.github.com>
2022-04-19 00:09:01 +0900

    remove salecount

--- contracts/KApmNftVoucherSale.sol
+++ contracts/KApmNftVoucherSale.sol
@@ -77,11 +77,6 @@ contract KApmNftVoucherSale is Ownable, IKApmNftVoucherSale {
         emit SetSaleLimit(saleLimit);
     }
 
-    function setSaleCount(uint256 _saleCount) public onlyOwner {
-        saleCount = _saleCount;
-         emit SetSaleCount(saleCount);
-    }
-
     function setSaleName(string memory _saleName) public onlyOwner {
         saleName = _saleName;
         emit SetSaleName(saleName);
