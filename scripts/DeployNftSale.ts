7bb418df3d14c3820e510df89dbc8e79bb73e436
jeon <46668752+john-ver@users.noreply.github.com>
2022-08-30 23:31:40 +0900

    test

--- scripts/DeployNftSale.ts
+++ scripts/DeployNftSale.ts
@@ -1,12 +1,16 @@
 import { ethers } from "hardhat";
 
+//새로운 KApmNftVoucherLimitSale 을 배포하는 계약
+
 async function main() {
+  console.log("Test")
   const testVault = '0xfee12041DCb1C482783c0F85c2f17B70E9fBB0f7';
   const testBPM = '0x092748a9a8226db6b0fdee8d7708abc4bcf53f99';
   const testNftVoucher = '0xc979Ebf3359EBCbD7925Be23320208925Cc6f71d';
 
   
-  const Greeter = await ethers.getContractFactory("Greeter");
+  
+  const nftVoucher = await ethers.getContractFactory("KApmNftVoucher");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
   await greeter.deployed();
